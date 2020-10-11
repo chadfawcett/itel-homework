97df464c6ec283c5f9cd8ab1c0ac7c757560a90c
Chad Fawcett <me@chadf.ca>
2020-10-11 15:23:38 -0700

    Clean up some unused code

--- src/data/fetcher.js
+++
@@ -1,8 +0,0 @@
-export default (...args) =>
-  fetch(...args, {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-host': 'veriphone.p.rapidapi.com',
-      'x-rapidapi-key': process.env.REACT_APP_VERIPHONE_KEY
-    }
-  }).then((res) => res.json())
